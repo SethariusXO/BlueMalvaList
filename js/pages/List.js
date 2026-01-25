91e3c0fcce56ac8420af22c776b5355840b8bd6c
SethariusXO <sethjbomers@gmail.com>
2026-01-25 13:42:57 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -52,7 +52,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">coins</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <p>{{ level.password || '0' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>

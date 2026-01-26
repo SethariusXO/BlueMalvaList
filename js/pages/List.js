026bb39950e4f816e7c6da544a25cc505c2e222a
SethariusXO <sethjbomers@gmail.com>
2026-01-26 17:06:47 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,9 +98,10 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3><a href="https://1drv.ms/w/c/b19b33f01ec2ee80/IQCkuBLLosSCQrfft3LSOAlIAfgDw8V2CGvZyMtJjBzWxHA?e=QGaUcf" target="_blank">Demon List Rules</a></h3>
+                    <h3><a href="https://1drv.ms/w/c/b19b33f01ec2ee80/IQCkuBLLosSCQrfft3LSOAlIAfgDw8V2CGvZyMtJjBzWxHA?e=QGaUcf" target="_blank">Demon List Rules</a></h3> {
                     text-decoration: underline;
                     color: var(--color-on-link);
+                    }
                     <p>
                         Click "Demon List Rules" to open the google doc
                     </p>
